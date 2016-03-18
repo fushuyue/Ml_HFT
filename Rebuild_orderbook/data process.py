69a714f59297a743ba9c4bfc02818a5fbbe0a4b5
fushuyue <fushuyue_apply@hotmail.com>
2016-03-17 21:31:16 -0500

    rebuild using pandas

--- Rebuild_orderbook/data process.py
+++ Rebuild_orderbook/data process.py
@@ -1,28 +1,43 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
 data = []
-trade = []
-trade1 = [[0,0]]
-order = []
-date = []
+tradep = []  # temporary storage of trade price
+tradev = []  # temporary storage of trade volume
+order = []  # storage for order data
+date = []  # storage for date
 date1 = []
-price = []
-a=[0,0]
+price = []  # storage for price
+lots = []  # storage for lots of every order
+trade = []
+a = [0,0,0,0]  # initialize
+
 def weighted_mean(x,y):
     temp = [x[i]*y[i] for i in range(len(x))]
     return sum(temp)/float((sum(y)))
 
+flag=0
 
-
-with open('ESZ5_20150917.txt') as input_file:
+with open('/Users/fushuyue/Dropbox/CME practicum/CMEdata/ESZ5_20150917.txt') as input_file:
     for line in input_file:
         if a[1] == 'TRADEREC':
             flag = True
-        else:
+            # trade data structure: 0 time 1 name; 2 quantity; 3 price; 4 volume; 5 flag
+        elif a[1] == 'BOOK10DEEPREC':
             flag = False
-
+        elif a[1] == 'IMPLIEDBOOKREC':
+            continue
+        # read data line by line
         a = line.strip().split(',')
-        if a[0] == '13:00:00.002':
-            break
-       '''
+        #if a[0] == '00:01:04.531':
+         #   break
+
+        if a[1] == 'TRADEREC':
+            # store trade price and volume
+            tradep.append(float(a[3]))
+            tradev.append(float(a[2]))
+
         if a[1] == 'BOOK10DEEPREC':
             temp = [int(k) for k in a[2:62]]
             date_temp = a[0][0:2]+a[0][3:5]+a[0][6:8]+a[0][9:12]
@@ -41,36 +56,49 @@ with open('ESZ5_20150917.txt') as input_file:
             mid_price = (ask_price[-1]*ask_size[-1]+bid_price[0]*bid_size[0])/float(ask_size[-1]+bid_size[0])
 
             weighted_price = (average_ask_p*sum(ask_size)+average_bid_p*sum(bid_size))/float(sum(bid_size)+sum(ask_size))
+            # 0=date; 1=bid price; 2=ask price;3=mid point; 4=spread; 5=imba; 6=normailize imba; 7=weighted price; 8=type;
 
-            order.append([date_temp, round(average_bid_p,5), round(average_ask_p,5), round(mid_price,5), spread,\
-                          volume_imbalance, round(normalized_imbalance,5), round(weighted_price,5)])
             if flag:
-                order[-1].append('trade')
+                temp1=sum(tradev)
+                lots.append(temp1)
+                temp2=weighted_mean(tradep,tradev)
+                order.append([date_temp, round(average_bid_p,5), round(average_ask_p,5),volume_imbalance, round(weighted_price,5),'trade',temp2 ])
+                tradep = []
+                tradev = []
+                trade.append([date_temp,temp1,temp2])
+
             else:
-                order[-1].append('submit/cancel')
-        '''
-        if a[1] == 'TRADEREC':
-            # 0 time 1 name; 2 quantity; 3 price; 4 volume; 5 flag
-            date_temp = a[0][0:2]+a[0][3:5]+a[0][6:8]+a[0][9:12]
-            trade.append([date_temp, a[1], int(a[2]), int(a[3])])
-            date.append(date_temp)
+                order.append([date_temp, round(average_bid_p,5), round(average_ask_p,5),volume_imbalance, round(weighted_price,5),'submit/cancel','NA'])
+print('finish reading')
 
-
+dtrade = pd.DataFrame(trade, columns=['date', 'volume', 'price'])
+dorder = pd.DataFrame(order, columns=['date', 'bid price', 'ask price', 'imba', 'weighted price', 'type', 'trade price'])
 '''
-for i in range(len(order)):
-    print(order[i])
+plt.plot(dtrade['price'])
+plt.ylim(198100,199000)
+plt.show()
 '''
 
-output=open("order.csv", "w")
+plt.hist(lots,bins=10,normed=1)
+plt.show()
+
+print('finish plot')
+
+dorder.to_csv('OrderWholeDay.txt')
+dtrade.to_csv('TradeWholeDay.txt')
+
+'''
+output=open("order.txt", "w")
 for i in range(len(order)):
     for k in range(len(order[i])):
         output.write(str(order[i][k]))
-        if k != len(order[1]):
+        if k != len(order[i])-1:
             output.write(",")
     output.write("\n")
 output.close()
 
-'''
+
+
 output1=open("trade.txt", "w")
 for i in range(len(trade)):
     for k in range(len(trade[i])):
@@ -78,39 +106,5 @@ for i in range(len(trade)):
         output1.write(",")
     output1.write("\n")
 output1.close()
-
-
-print('finish reading')
-n = len(trade)
-print(trade[-1][0])
-i = 0
-print(n)
-# merge same trade together using weighted average
-for f in range(n):
-    if i >= n-1:
-        break
-    k = date.count(trade[i][0])
-    temp = 0
-    weights = 0
-    for j in range(k):
-        temp = temp+trade[i+j][2]*trade[i+j][3]
-        weights = weights+trade[i+j][2]
-    trade1.append([trade[i][0], temp/weights, weights])
-    date1.append(trade[i][0])
-    i = i+k
-    if i % 1000 == 0:
-        print(trade[i][0])
-
-output1=open("processed_trade.txt", "w")
-for i in range(len(trade1)):
-    for k in range(len(trade1[i])):
-        output1.write(str(trade1[i][k]))
-        output1.write(",")
-    output1.write("\n")
-output1.close()
-
-
-
-print('finish merging')
-
 '''
+
