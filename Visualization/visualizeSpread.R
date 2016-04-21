8273233d92ad075a5b19959ddc748f546a368503
fushuyue <fushuyue_apply@hotmail.com>
2016-04-21 11:22:40 -0500

    add prediction outcome

--- Visualization/visualizeSpread.R
+++ Visualization/visualizeSpread.R
@@ -1,4 +1,4 @@
-setwd('~/Dropbox/CME practicum/Apr 22nd/SVM/testing_data&result/')
+setwd('~/Dropbox/CME practicum/Apr 22nd/SVM/testing set/')
 tdata=read.csv("with1_80_20_testing_data.csv")
 data=read.csv('~/Desktop/python/esOrderWholeDay.txt')
 time=tdata$time.5.n.
@@ -13,10 +13,22 @@ fu = cbind(fu,accr)
 
 fu$time.5.n = fu$time.5.n/1000
 df$time = df$time/1000
+fu$real1 = ifelse(fu$real1 == 1, 'firebrick1', 'green')
 
-p = ggplot()+geom_line(data = df,aes(x = time/1000,y = best.ask+30,colour = best.ask.size,size = 5))+xlab('time')+ylab('price')
 
-p = p+geom_line(data = df,aes(x = time/1000,y = best.bid-30,colour = best.bid.size,size = 5))
+p = ggplot()+geom_line(data = df,aes(x = time/10000,y = best.ask+30,colour = best.ask.size),size = 3)+
+    xlab('time')+ylab('price')
+
+p = p+geom_line(data = df,aes(x = time/10000,y = best.bid-30,colour = best.bid.size), size = 3)
+
+p = p + scale_color_gradient(name = 'liquidity of \nask and bid',low = 'pink', high = 'red')
+
+fuup = fu[fu$real1 == 'firebrick1',]
+fudown = fu[fu$real1 == 'green',]
+
+p1 = p + geom_point(data = fuup,aes(x = time.5.n/10000, y = trade_price1, alpha = 0.8, shape = factor(accr)),colour = 'blue',size = 5)
+p1 = p1 + geom_point(data = fudown,aes(x = time.5.n/10000, y = trade_price1, alpha = 0.8, shape = factor(accr)),colour = 'black',size = 5)
+p1 = p1  + guides(alpha = F) + scale_shape_discrete(name = 'prediction outcome', solid = F) + guides(alpha=F)
 
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
   library(grid)
